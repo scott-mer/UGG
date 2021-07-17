345922f8be29f393816d1ed8d61f99463d83434f
scott-mer <scott.c.mer@gmail.com>
2021-07-17 15:08:52 -0700

    Added loading in card from array

--- deck.lua
+++ deck.lua
@@ -3,25 +3,58 @@
 
 -- Card class
 -- holds the information of a single card
+Cards = {
+    {
+        name = "punch",
+        damage = 5,
+        block = 0,
+        time = 2
+    },
+    {
+        name = "block",
+        damage = 0,
+        block = 0,
+        time = 5
+    }
+}
+
 Card = {
     id = 0,
+    name = "",
     damage = 0,
     block = 0,
     time = 0
 }
-function Card:new(o, id, damage, block, time)
+function Card:add(o, id, name)
     o = o or {}
     setmetatable(o, self)
     self.__index = self
     self.id = 1
-    self.damage = 1
-    self.block = 1
-    self.time = 1
+    
+    -- search for card in present cards
+    isFound = false
+    for _,v in pairs(Cards) do
+        if(v["name"] == name)
+        then
+            self.name = v["name"]
+            self.damage = v["damage"]
+            self.block = v["block"]
+            self.time = v["time"]
+            isFound = true
+        end
+    end
+
+    -- check that card was found
+    if(not isFound)
+    then
+        print("ERROR: Could not find card name")
+    end
+
     return o
 end
 
-function Card:printCard()
-    print(self.id)
+function Card:print()
+    print("Name:", self.name, "\nDamage:", self.damage, "\nBlock:", self.block, "\nTime:", self.time)
 end
 
 -- Deck class
@@ -36,8 +69,8 @@ function Deck:new(o, count)
     self.__index = self
     for i = 1, count, 1
     do
-        table.insert(self.cards, Card:new(nil, i))
+        table.insert(self.cards, Card:add(nil, i, "block"))
+        print(self.cards[i]:print())
     end
-    print(self.cards[1]:printCard())
     return o
 end
