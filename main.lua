5be69864384f8a759ce874af8f968f8040e2d20a
scott-mer <scott.c.mer@gmail.com>
2021-07-17 17:21:03 -0700

    Added demo code to main

--- main.lua
+++ main.lua
@@ -5,7 +5,13 @@ require("utilities")
 require("deck")
 
 function love.load()
-    Deck:new(nil, 5)
+    deck1 = deck:new({"jab", "punch", "punch", "block"})
+    deck2 = deck:new({"block", "punch", "jab", "block"})
+
+    print("Deck1")
+    deck1:print()
+    print("Deck2")
+    deck2:print()
 end
 
 function love.update(dt)
